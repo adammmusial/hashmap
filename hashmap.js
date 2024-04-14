e8ccf558285cd7b80fcc46801aba83b898a08c57
adammmusial <musialadam83@gmail.com>
2024-04-14 20:58:16 +0800

    feat: add set() function to add key-value pais to hash table

--- hashmap.js
+++ hashmap.js
@@ -1,18 +1,54 @@
-class HashMap{
+class HashMap {
     constructor() {
         this.buckets = []
     }
 
     hash(key) {
         let hashCode = 0;
-           
-        const primeNumber = 31;
-        
-        for (let i = 0; i < key.length; i++) {
-          hashCode = primeNumber * hashCode + key.charCodeAt(i);
-        }
-     
-        return hashCode;
-      } 
 
+        const primeNumber = 31;
+
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    }
+
+    set(key, value) {
+
+        const index = this.hash(key);
+
+        // Check if there is already a bucket (array) at the calculated index in the hash table.
+        // If there isn't a bucket (i.e., it's undefined), we need to initialize one.
+        if (!this.buckets[index]) {
+            this.buckets[index] = [];
+        }
+
+        const bucket = this.buckets[index];
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i][0] === key) {
+                bucket[i][1] = value;
+                return;
+            }
+        }
+        bucket.push([key, value])
+
+    }
+
+    get(key){
+        const index = this.hash(key)
+        const buckets = this.buckets[index]
+        if (!buckets){
+            return null
+        }
+
+        for (let i = 0; i < buckets.length; i++){
+            if (buckets[i][0] === key){
+                return buckets[i][1];
+            }
+        }
+
+        return null
+    }
 }
