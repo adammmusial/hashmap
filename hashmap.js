aef2c334b0630f9e77ec8ca18f82e18c893408b8
adammmusial <musialadam83@gmail.com>
2024-04-14 20:20:05 +0800

    feat: add hashing function

--- hashmap.js
+++ hashmap.js
@@ -1,5 +1,18 @@
-class Hashmap{
+class HashMap{
     constructor() {
         this.buckets = []
     }
+
+    hash(key) {
+        let hashCode = 0;
+           
+        const primeNumber = 31;
+        
+        for (let i = 0; i < key.length; i++) {
+          hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+     
+        return hashCode;
+      } 
+
 }
