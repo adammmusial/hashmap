0276987755c05581fce1cfa7c3f835ca2bcb8d9f
adammmusial <musialadam83@gmail.com>
2024-04-14 23:23:27 +0800

    feat: add test lines

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 import HashMap from './hashmap.js'
+import MapProto from './hashmap2.js'
 
 const map = new HashMap()
+const map2 = new MapProto()
 
-map.set("Walter","Jonsons")
+console.log(map)
 
-console.log(map.count())
+const key1 = "jp2"
+const val1 = "420"
+const key2 = "jp3"
+const val2 = "421"
+
+map.set(key1,val1)
+map.set(key2,val2)
+console.log(map)
+console.log(map.values())
+console.log(map.entries())
+
+
+map2.set(key1,val1)
+map2.set(key2,val2)
+console.log(map2)
+console.log(map2.values())
+console.log(map2.entries())
+
+
+
+
